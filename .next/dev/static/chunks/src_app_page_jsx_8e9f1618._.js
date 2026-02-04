1efb0902cd329e2e343249e22c168b9ecdfe6c2e
Arun <arun@zinavo.in>
2026-02-04 10:29:57 +0530

    msg

--- .next/dev/static/chunks/src_app_page_jsx_8e9f1618._.js
+++ .next/dev/static/chunks/src_app_page_jsx_8e9f1618._.js
@@ -1,10 +1,8 @@
-(
-  globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])
-).push({
-  script: typeof document === "object" ? document.currentScript : undefined,
-  chunks: [
-    "static/chunks/node_modules_2b0cd080._.js",
-    "static/chunks/src_5f40bdd6._.js",
-  ],
-  source: "dynamic",
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_2b0cd080._.js",
+  "static/chunks/src_5f40bdd6._.js"
+],
+    source: "dynamic"
 });
